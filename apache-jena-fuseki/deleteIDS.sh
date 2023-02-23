10a4afff8ca04eb070c44093cdc545abb5423fe7
Lejarazu Marquina, Adelaida <adelaida.lejarazu@tecnalia.com>
2023-02-23 08:52:31 +0100

    added fuseki

---
+++ apache-jena-fuseki/deleteIDS.sh
@@ -0,0 +1,218 @@
+	
+ ./bin/s-delete  http://localhost:3030/dataset  http://localhost:3030/dataset/ids/develop/model/communication/OperationType.ttl
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/communication/MessageExchangePattern.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/communication/ApiDocumentType.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/communication/FaultPropagationRule.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/content/references/owl-time-20171019.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/metamodel/Validation.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/metamodel/Annotation.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/context/Event.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/infrastructure/AppStore.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/infrastructure/IdentityProvider.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/infrastructure/ClearingHouse.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/content/references/dcat-20140116.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/infrastructure/ParIS.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/infrastructure/DAPS.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/content/references/ma-ont-20120209.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/participant/references/org-20140412.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/content/references/void-20110303.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/content/references/data-cube-20140116.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/content/references/csvw-20170523.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/content/references/adms-20130801.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/content/references/dctype-20120614.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/quality/references/dqv-20161215.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/model/communication/DataType.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/docs/provenance/provenance-en.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/Ontology.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/metamodel/View.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/taxonomies/Parameter.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/views/GlossaryView.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/APP2.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/APPSTORE1.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/Unit.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/DATA1.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/CONN2.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/BROKER1.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/PART1.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/domain-specific-semantics-using-SHACL/TEMP_SHACL.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/domain-specific-semantics-using-SHACL/TEMP_SAMPLE_DATA.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/TEXT_RESOURCE.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/domain-specific-semantics-using-SHACL/TEMP_RESOURCE.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/License.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/MediaType.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/CONN1.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/Action.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/TRUSTED_CONNECTOR.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/DATA3.ttl  
+	
+./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/Concept.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/INTER1.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/INTER2.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/PART2.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/INTER3.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/Protocol.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/SecurityGuarantee.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/CertificationLevel.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/PART3.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/DATA2.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/rdf-validation-using-shacl/cli/conn_self_description.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/examples/APP1.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/SortOrder.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageAgreementUntilDeletionTemplate.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/PageReference.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/Standard.ttl  
+
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/IndustrialClassification.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/TimeRestrictedUsageAgreementTemplate.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageRequestLogAccessTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset  http://localhost:3030/dataset/ids/develop/codes/LeftOperand.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageOfferAfterPaymentTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageRequestByRoleTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/TimelRestrictedUsageOfferTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageAgreementAfterEventTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageAgreementAfterPaymentTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageRequestNUsagesTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageOfferUntilDeletionTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageOfferByRoleTemplate.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageRequestAfterPaymentTemplate.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/QueryLanguage.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/BinaryOperators.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/MinimalOfferTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset  http://localhost:3030/dataset/ids/develop/codes/UsageOfferAfterEventTemplate.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/TimeRestrictedUsageRequestTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageOfferLogAccessTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/SpatialRestrictedUsageOfferTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/RequestTemplate.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/MinimalRequestTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset  http://localhost:3030/dataset/ids/develop/codes/AccessOfferToSecureConsumerTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/AccessRequestToSecureConsumerTemplate.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/QueryTarget.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/AccessAgreementToSecureConsumerTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageAgreementForPurposeTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageAgreementByRoleTemplate.ttl  
+	
+  ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/FaultPropagationRule.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/AgreementTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageAgreementLogAccessTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageAgreementNUsagesTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/Language.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/MessageExchangePattern.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/SpatialRestrictedUsageAgreementTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageRequestAfterEventTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/RejectionReason.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageRequestUntilDeletionTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/OfferTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageOfferForPurposeTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageRequestForPurposeTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/ContentType.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/SpatialRestrictedUsageRequestTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/QueryScope.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/TokenFormat.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/UsageOfferNUsagesTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/OfferByRoleTemplate.ttl  
+	
+ ./bin/s-delete  http://localhost:3030/dataset http://localhost:3030/dataset/ids/develop/codes/KeyType.ttl  
